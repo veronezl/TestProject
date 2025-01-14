c628e05fe836b1a41d3c0b3a0791f8eb52da4a15
veronezl <lucasveronez.dev@gmail.com>
2025-01-14 16:39:29 -0300

    Trabalhando com tipos de dados parte 2

--- Program.cs
+++ Program.cs
@@ -301,6 +301,7 @@ Console.WriteLine($"'o' appears {letterCount} times.");
 
 // Descobrir tipos integrais -------------------------------------------------------------------
 
+/*
 Console.WriteLine("Signed integral types:");
 
 Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
@@ -316,3 +317,18 @@ Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
 Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
 Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
 
+Console.WriteLine("");
+Console.WriteLine("Floating point types:");
+Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+*/
+
+int[] ref_A = new int[1];
+ref_A[0] = 2;
+int[] ref_B = ref_A;
+ref_B[0] = 5;
+
+Console.WriteLine("--Reference Types--");
+Console.WriteLine($"ref_A[0]: {ref_A[0]}");
+Console.WriteLine($"ref_B[0]: {ref_B[0]}");
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f544a8ba96a5395edd7e715b223540570d9f0829")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+971c15a7a089faaa6d20e3dfdfd4e583f352ea59")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
