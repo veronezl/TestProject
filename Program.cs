b2282a0e681eae0e5adbd86ddbcb406968c0aa6c
veronezl <lucasveronez.dev@gmail.com>
2025-01-20 09:38:29 -0300

    Desafio para extrair, substituir e remover os dados

--- Program.cs
+++ Program.cs
@@ -910,6 +910,7 @@ Found WITH using startPosition 5:  (find) the {opening symbols}
 
 // Outro exemplo de IndexOfAny():
 
+/*
 string message = "(What if) I have [different symbols] but every {open symbol} needs a [matching closing symbol]?";
 
 // The IndexOfAny() helper method requires a char array of characters. 
@@ -960,5 +961,72 @@ while (true)
     int length = closingPosition - openingPosition;
     Console.WriteLine(message.Substring(openingPosition, length));
 }
+*/
 
 // Output: different symbols, open symbol, matching closing symbol
+
+// Exercício – Usar os métodos Remove() e Replace(): 
+
+// Usar o método Remove()
+
+// Remover caracteres em locais específicos de uma cadeia de caracteres:
+
+/*
+string data = "12345John Smith          5000  3  ";
+string updatedData = data.Remove(5, 20);
+Console.WriteLine(updatedData);
+*/
+
+// Output: 123455000  3
+
+// Usar o método Replace(): 
+
+// Remover caracteres, independentemente de onde eles aparecem em uma cadeia de caracteres: 
+
+/*
+string message = "This--is--ex-amp-le--da-ta";
+message = message.Replace("--", " ");
+message = message.Replace("-", "");
+Console.WriteLine(message);
+*/
+
+// Exercício: conclua um desafio para extrair, substituir e remover os dados de uma cadeia de 
+// caracteres de entrada
+
+/*
+const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+// Your work here
+
+// Extract the "quantity"
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+
+int quantityStart = input.IndexOf(openSpan) + openSpan.Length; // + length of <span> so index at end of <span> tag
+int quantityEnd= input.IndexOf(closeSpan);
+int quantityLength = quantityEnd - quantityStart;
+quantity = input.Substring(quantityStart, quantityLength);
+quantity = $"Quantity: {quantity}";
+
+// Set output to input, replacing the trademark symbol with the registered trademark symbol
+const string tradeSymbol = "&trade;";
+const string regSymbol = "&reg;";
+output = input.Replace(tradeSymbol, regSymbol);
+
+// Remove the opening <div> tag
+const string openDiv = "<div>";
+int divStart = output.IndexOf(openDiv);
+output = output.Remove(divStart, openDiv.Length);
+
+// Remove the closing </div> tag and add "Output:" to the beginning
+const string closeDiv = "</div>";
+int divCloseStart = output.IndexOf(closeDiv);
+output = "Output: " + output.Remove(divCloseStart, closeDiv.Length);
+
+Console.WriteLine(quantity);
+Console.WriteLine(output);
+*/
+
