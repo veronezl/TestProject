f0bb0f88d2872047b7ef66266e83093c8d31d63b
veronezl <lucasveronez.dev@gmail.com>
2025-01-30 09:47:09 -0300

    Estudo de métodos - parte 5

--- Program.cs
+++ Program.cs
@@ -1425,6 +1425,7 @@ void ShowRSVPs()
 
 // Exercício – Concluir o desafio de exibir endereços de email:
 
+/*
 string[,] corporate = 
 	{
 		{"Robert", "Bavin"}, {"Simon", "Bright"},
@@ -1455,3 +1456,74 @@ string[,] corporate =
 		email = email.ToLower();
 		Console.WriteLine($"{email}@{domain}");
 	}
+*/
+
+// Crie métodos C# que retornem valores -------------------------------------------------------
+
+// Entender a sintaxe do tipo de retorno:
+
+// Usar métodos para calcular o preço total de compra
+
+
+/*double total = 0;
+double minimumSpend = 30.00;
+
+double[] items = { 15.97, 3.50, 12.25, 22.99, 10.98 };
+double[] discounts = { 0.30, 0.00, 0.10, 0.20, 0.50 };
+
+for (int i = 0; i < items.Length; i++)
+{
+    total += GetDiscountedPrice(i);
+}
+
+if (TotalMeetsMinimum())
+{
+    total -= 5.00;
+}
+
+Console.WriteLine($"Total: ${FormatDecimal(total)}");
+
+double GetDiscountedPrice(int itemIndex)
+{
+    // Calculate the discounted price of the item
+    double result = items[itemIndex] * (1 - discounts[itemIndex]);
+    return result;
+}
+
+bool TotalMeetsMinimum()
+{
+    // Check if the total meets the minimum
+    return total >= minimumSpend;
+}
+
+string FormatDecimal(double input)
+{
+    // Format the double so only 2 decimal places are displayed
+    return input.ToString().Substring(0, 5);
+}
+*/
+
+// Retornar números de métodos:
+
+// Criar um método que retorna um inteiro:
+
+// Converter dolar para vnd->
+
+double usd = 23.73;
+int vnd = UsdToVnd(usd);
+
+Console.WriteLine($"${usd} USD = ${vnd} VND");
+
+Console.WriteLine($"${vnd} VND = ${VndToUsd(vnd)} USD");
+
+int UsdToVnd(double usd) 
+{
+    int rate = 23500;
+    return (int)(rate * usd);
+}
+
+double VndToUsd(int vnd) 
+{
+    double rate = 23500;
+    return vnd / rate;
+}
