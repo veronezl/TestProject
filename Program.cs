adf50d7378623d8151a85faee9ba8d182ebe0364
veronezl <lucasveronez.dev@gmail.com>
2025-02-05 08:35:04 -0300

    Desafio do modulo métodos concluído

--- Program.cs
+++ Program.cs
@@ -1754,36 +1754,69 @@ string WinOrLose(int roll, int target)
 
 // Projeto guiado – Planejar uma visita ao zoológico =======================================================
 
+/*
 using System;
-using System.Security.Cryptography;
 
-string[] pettingZoo =
+string[] pettingZoo = 
 {
-    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese",
-    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese", 
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
 
-// RandomizeAnimals();
+PlanSchoolVisit("School A");
+PlanSchoolVisit("School B", 3);
+PlanSchoolVisit("School C", 2);
 
-// string[,] group = AssignGroup();
+void PlanSchoolVisit(string schoolName, int groups = 6) 
+{
+    RandomizeAnimals(); 
+    string[,] group1 = AssignGroup(groups);
+    Console.WriteLine(schoolName);
+    PrintGroup(group1);
+}
 
-System.Console.WriteLine("School A");
-
-// PrintGroup(group);
-
-void RandomizeAnimals()
+void RandomizeAnimals() 
 {
     Random random = new Random();
 
-    for (int i = 0; i < pettingZoo.Length; i++)
+    for (int i = 0; i < pettingZoo.Length; i++) 
     {
         int r = random.Next(i, pettingZoo.Length);
 
-        string temp = pettingZoo[i];
-        pettingZoo[i] = pettingZoo[r];
-        pettingZoo[r] = temp;
+        string temp = pettingZoo[r];
+        pettingZoo[r] = pettingZoo[i];
+        pettingZoo[i] = temp;
+    }
+}
+
+string[,] AssignGroup(int groups = 6) 
+{
+    string[,] result = new string[groups, pettingZoo.Length/groups];
+    int start = 0;
+
+    for (int i = 0; i < groups; i++) 
+    {
+        for (int j = 0; j < result.GetLength(1); j++) 
+        {
+            result[i,j] = pettingZoo[start++];
+        }
     }
 
-
+    return result;
 }
+
+void PrintGroup(string[,] groups) 
+{
+    for (int i = 0; i < groups.GetLength(0); i++) 
+    {
+        Console.Write($"Group {i + 1}: ");
+        for (int j = 0; j < groups.GetLength(1); j++) 
+        {
+            Console.Write($"{groups[i,j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+*/
+
